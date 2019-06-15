8d55dab87fcba3a14693c6d1c4cac2c57fb7bcd1
Artmen_V <viliutich.artem@gmail.com>
2019-06-15 20:06:10 +0700

    useSetState

--- src/App.js
+++ src/App.js
@@ -5,14 +5,32 @@ const App = () => {
 
   const [count, setCount] = useState(0)
 
+  const [isOn, setIsOn] = useState(false)
+
   const incrementCount = () => {
-setCount(count +1)
+    setCount(count +1)
+  }
+
+    const toggleLight = () => {
+      setIsOn(prevIsOn => !prevIsOn)
     }
 
     return (
       <>
         <p>{ count }</p>
         <button onClick={incrementCount} className='btn'>Click Me 7 times!!!</button>
+
+        <h2>Toggle light</h2>
+        <div
+          style={{
+            height: '50px',
+            width: '50px',
+            background: isOn ? 'yellow' : 'grey',
+            padding: '25px',
+            margin: '25px'
+            }}
+            onClick={toggleLight}
+        ></div>
       </>
     );
   }
